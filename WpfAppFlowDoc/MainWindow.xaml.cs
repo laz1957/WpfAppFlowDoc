3da06267249ba66c4264b5830a7d4c3a4dc72d70
laz1957 <ulazarev@kait20.ru>
2024-11-05 09:52:10 +0300

    Добавьте файлы проекта.

---
+++ WpfAppFlowDoc/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppFlowDoc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 w = new Window1();
+            w.Show();
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 w = new Window2();
+            w.Show();
+        }
+    }
+}
---
+++ WpfAppFlowDoc/Window2.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppFlowDoc
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        string path = "mydoc.xaml";
+        public Window2()
+        {
+            InitializeComponent();
+        }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open(path, FileMode.Create))
+            {
+                if (docViewer.Document != null)
+                {
+                    System.Windows.Markup.XamlWriter.Save(docViewer.Document, fs);
+                    MessageBox.Show("Файл сохранен");
+                }
+            }
+        }
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            docViewer.ClearValue(FlowDocumentScrollViewer.DocumentProperty);
+        }
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open(path, FileMode.Open))
+            {
+                FlowDocument document = System.Windows.Markup.XamlReader.Load(fs) as FlowDocument;
+                if (document != null)
+                    docViewer.Document = document;
+            }
+        }
+    }
+}
